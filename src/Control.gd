1c78af41767c79aa34c3ce0744b262e5e5b0f50c
Soygargo <nejatseninci@gmail.com>
2019-08-24 19:39:41 +0300

    added pause screen

---
+++ src/Control.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		var new_pause_state = not get_tree().paused
+		get_tree().paused = new_pause_state
+		visible = new_pause_state	
+
+func _on_TextureButton1_pressed():
+	var new_pause_state = not get_tree().paused
+	get_tree().paused = new_pause_state
+	visible = new_pause_state	
+
+
+func _on_TextureButton2_pressed():
+	get_tree().quit()
